1893ac5f80044667e49dde744531ada9c1e8a2df
pascalpoizat <pascal.poizat@lip6.fr>
2017-12-04 11:11:33 +0100

    initial commit

---
+++ .bashrc
@@ -0,0 +1,6 @@
+alias rm='rm -i'
+alias cp='cp -i'
+alias mv='mv -i'
+alias git=hub
+export PATH=$PATH:$HOME/.local/bin
+
---
+++ saveconfig.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export DEST=~/Downloads/CONFIG/configuration-files
+cp -f ~/.spacemacs $DEST
+cp -f ~/.gitconfig $DEST
+cp -f ~/.gitignore_global $DEST
+cp -f ~/.bashrc $DEST
+cp -f ~/.bash_profile $DEST
+cp -f ~/.zshrc $DEST
+echo "Files saved to $DEST, do no forget to run git."
+
