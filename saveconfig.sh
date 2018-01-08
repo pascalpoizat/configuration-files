f748a7c8ad9ad476fad5970ca96db277eadc8f31
pascalpoizat <pascal.poizat@lip6.fr>
2018-01-08 16:45:31 +0100

    chore: add script and config file for web page synchronizing

--- saveconfig.sh
+++ saveconfig.sh
@@ -9,5 +9,7 @@ cp -f ~/.bash_profile $DEST
 cp -f ~/.zshrc $DEST
 cp -f ~/.zshenv $DEST
 cp -f ~/.czrc $DEST
+cp -f ~/.local/bin/syncpages $DEST
+cp -f ~/rsync_exclude_file.txt $DEST
 echo "Files saved to $DEST, do no forget to run git."
 
